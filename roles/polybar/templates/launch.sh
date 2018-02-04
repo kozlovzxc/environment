a1937eef0e6c6172157707eb9883f5b40b417ca1
Kozlov Nikita <kozlovzxc@gmail.com>
2018-02-04 12:55:24 +0300

    new polybar theme

--- roles/polybar/templates/launch.sh
+++ roles/polybar/templates/launch.sh
@@ -6,8 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar example &
-
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR="$m" polybar --reload main_bar &
+  done
+else
+  polybar --reload main_bar &
+fi
 
 echo "Bars launched..."
